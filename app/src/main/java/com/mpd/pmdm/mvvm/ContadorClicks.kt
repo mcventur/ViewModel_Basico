3ee1a93a4edeee865700e4e80b51141462e6baf8
mcventur <marcelinopenide@gmail.com>
2024-01-15 20:37:56 +0100

    LiveDaTA Y ViewModelProvider.Factory

--- app/src/main/java/com/mpd/pmdm/mvvm/ContadorClicks.kt
+++ app/src/main/java/com/mpd/pmdm/mvvm/ContadorClicks.kt
@@ -1,12 +1,26 @@
 package com.mpd.pmdm.mvvm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-class ContadorClicks: ViewModel() {
-    var cuentaClicks: Int = 0
+class ContadorClicks(valorContador: Int): ViewModel() {
+    private val _cuentaClicks = MutableLiveData<Int>(valorContador)
+    val cuentaClicks: LiveData<Int> = _cuentaClicks
 
-    fun incrementa() = cuentaClicks++
+    fun incrementa() {
+        _cuentaClicks.value = cuentaClicks.value?.plus(1)
+    }
 
-    fun decrementa() = cuentaClicks--
+    fun decrementa() {
+        _cuentaClicks.value = cuentaClicks.value?.minus(1)
+    }
+}
 
+class ContadorClickFactory(val valor: Int): ViewModelProvider.Factory{
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return ContadorClicks(valor) as T
+    }
 }
--- app/src/main/java/com/mpd/pmdm/mvvm/MainActivity.kt
+++ app/src/main/java/com/mpd/pmdm/mvvm/MainActivity.kt
@@ -7,26 +7,30 @@ import com.mpd.pmdm.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val contador: ContadorClicks by viewModels()
+    private val contador: ContadorClicks by viewModels{
+        ContadorClickFactory(5)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        contador.cuentaClicks.observe(this){
+            actualizaContador()
+        }
+
         actualizaContador()
 
         binding.btnIncrementa.setOnClickListener {
             contador.incrementa()
-            actualizaContador()
         }
 
         binding.btnResta.setOnClickListener {
             contador.decrementa()
-            actualizaContador()
         }
     }
 
     private fun actualizaContador() {
-        binding.tvContador.text = contador.cuentaClicks.toString()
+        binding.tvContador.text = contador.cuentaClicks.value.toString()
     }
 }
